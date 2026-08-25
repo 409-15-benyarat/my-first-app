7c84f82a8643ba8d38566436e919fbbbc3e663d0
409-15-benyarat <benyarat.nanana@gmail.com>
2026-08-25 14:26:00 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -130,4 +130,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวเบญญารัตน์ นาวิก เลขที่15 ม.4/9")
